0f60a56753cbb0ae2713452ff0fd10ee13cd7ee0
cbuscaron <cbuscaron@gmail.com>
2015-11-07 15:06:57 -0800

    relationship extraction service

---
+++ EmiApp/server.py
@@ -0,0 +1,24 @@
+import os
+from jsonrpc import ServiceProxy
+try:
+  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+  from SocketServer import TCPServer as Server
+except ImportError:
+  from http.server import SimpleHTTPRequestHandler as Handler
+  from http.server import HTTPServer as Server
+
+# Read port selected by the cloud for our application
+PORT = int(os.getenv('PORT', 8000))
+# Change current directory to avoid exposure of control files
+os.chdir('static')
+
+httpd = Server(("", PORT), Handler)
+try:
+  print("Start serving at port %i" % PORT)
+  s = ServiceProxy("http://localhost/services/test.py")
+  print s.echo("foobar")
+  httpd.serve_forever()
+except KeyboardInterrupt:
+  pass
+httpd.server_close()
+
